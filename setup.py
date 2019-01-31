865e95f14f143407dc5f94a995f0ab145a8e0e72
Antonis Christofides <antonis@antonischristofides.com>
2019-01-31 16:54:25 +0200

    Black

--- setup.py
+++ setup.py
@@ -2,46 +2,42 @@
 
 from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', "htimeseries"]
+requirements = ["Click>=6.0", "htimeseries"]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Antonis Christofides",
-    author_email='antonis@antonischristofides.com',
+    author_email="antonis@antonischristofides.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     description="Aggregates htimeseries to larger steps",
-    entry_points={
-        'console_scripts': [
-            'haggregate=haggregate.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["haggregate=haggregate.cli:main"]},
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='haggregate',
-    name='haggregate',
-    packages=find_packages(include=['haggregate']),
+    keywords="haggregate",
+    name="haggregate",
+    packages=find_packages(include=["haggregate"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/openmeteo/haggregate',
-    version='0.1.0.dev0',
+    url="https://github.com/openmeteo/haggregate",
+    version="0.1.0.dev0",
     zip_safe=False,
 )
