9f9f2eecaa2a35dbb1238d5ecb20b44d5d04684b
Antonis Christofides <antonis@antonischristofides.com>
2020-02-23 23:56:33 +0200

    Use Python 3.7 and htimeseries 3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=7.0,<8", "htimeseries>=2.0.3,<3"]
+requirements = ["Click>=7.0,<8", "htimeseries>=3,<4"]
 
 setup_requirements = ["cython>=0.29,<0.30"]
 
@@ -72,12 +72,11 @@ setup(
     author="Antonis Christofides",
     author_email="antonis@antonischristofides.com",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Aggregates htimeseries to larger steps",
     entry_points={"console_scripts": ["haggregate=haggregate.cli:main"]},
